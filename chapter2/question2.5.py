43440c69a32d2e490c016c63aff6903ef2bde96c
Shichao An <shichao.an@nyu.edu>
2014-04-17 01:53:32 -0400

    Finished q2.5

--- chapter2/question2.5.py
+++ chapter2/question2.5.py
@@ -45,6 +45,47 @@ def add_lists(l1, l2):
     return res
 
 
+def get_length(head):
+    return len(restore_list(head))
+
+
+def pad_list(head, n):
+    """Pad a linked list with zeros before head"""
+    for i in range(n):
+        new_head = ListNode(0)
+        new_head.next = head
+        head = new_head
+    return head
+
+
+def add_lists2_aux(l1, l2, res):
+    """Add two equal-size linked lists that store digits in forward order"""
+    if l1 is None and l2 is None:
+        return 0
+    carry = add_lists2_aux(l1.next, l2.next, res)
+    data = (l1.data + l2.data + carry) % 10
+    carry = 1 if l1.data + l2.data + carry >= 10 else 0
+    res.insert(0, data)
+    return carry
+
+
+def add_lists2(l1, l2):
+    len1 = get_length(l1)
+    len2 = get_length(l2)
+    if len1 < len2:
+        l1 = pad_list(l1, len2 - len1)
+    else:
+        l2 = pad_list(l2, len1 - len2)
+    t = []
+    carry = add_lists2_aux(l1, l2, t)
+    res = create_list(t)
+    if carry > 0:
+        new_head = ListNode(1)
+        new_head.next = res
+        res = new_head
+    return res
+
+
 def _test():
     pass
 
@@ -55,11 +96,19 @@ def _print():
     r1 = reverse_list(l1)
     print(restore_list(r1))
     n1 = [6, 1, 7]
-    n2 = [2, 9]
+    n2 = [2, 9, 5]
     nl1 = create_list(n1)
     nl2 = create_list(n2)
-    r = add_lists(nl1, nl2)
+    r = add_lists2(nl1, nl2)
     print(restore_list(r))
+    n3 = [1, 2, 3]
+    n4 = [2, 3, 4]
+    nl3 = create_list(n3)
+    nl4 = create_list(n4)
+    nl5 = create_list([9, 3, 4, 7])
+    nl6 = create_list([9, 7, 2])
+    rr = add_lists2(nl5, nl6)
+    print(restore_list(rr))
 
 
 if __name__ == '__main__':
