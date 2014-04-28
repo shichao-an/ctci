b0ac9e419a52e0e128cd03e8e8d1de1d9c71b8f2
Shichao An <shichao.an@nyu.edu>
2014-04-28 00:32:18 -0400

    Added alts

--- chapter9/question9.6.py
+++ chapter9/question9.6.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 
 
-def generate_parens_aux(left, right, cand, res):
+def generate_parens1_aux(left, right, cand, res):
     """
+    Backtrack: prune branch return
     `left` and `right` are remaining left and right parens
     """
     if left > right or left < 0:
@@ -10,14 +11,34 @@ def generate_parens_aux(left, right, cand, res):
     elif left == 0 and right == 0:
         res.append(cand)
     else:
-        generate_parens_aux(left - 1, right, cand + '(', res)
-        generate_parens_aux(left, right - 1, cand + ')', res)
+        generate_parens1_aux(left - 1, right, cand + '(', res)
+        generate_parens1_aux(left, right - 1, cand + ')', res)
 
 
-def generate_parens(n):
+def generate_parens1(n):
     res = []
     cand = ''
-    generate_parens_aux(n, n, cand, res)
+    generate_parens1_aux(n, n, cand, res)
+    return res
+
+
+def generate_parens2_aux(left, right, cand, res):
+    """
+    Backtrack: prune by conditional branch
+    """
+    if left == 0 and right == 0:
+        res.append(cand)
+    else:
+        if left > 0:
+            generate_parens2_aux(left - 1, right, cand + '(', res)
+        if right > left:
+            generate_parens2_aux(left, right - 1, cand + ')', res)
+
+
+def generate_parens2(n):
+    res = []
+    cand = ''
+    generate_parens2_aux(n, n, cand, res)
     return res
 
 
@@ -26,14 +47,12 @@ def _test():
 
 
 def _print():
-    r1 = generate_parens(1)
-    r2 = generate_parens(2)
-    r3 = generate_parens(3)
-    r4 = generate_parens(4)
-    print(r1)
-    print(r2)
-    print(r3)
-    print(r4)
+    for i in range(1, 5):
+        res = generate_parens1(i)
+        print(res)
+    for i in range(1, 5):
+        res = generate_parens2(i)
+        print(res)
 
 
 if __name__ == '__main__':
