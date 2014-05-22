8c015e44d9e33645696e66847ea3705b884ccd48
Shichao An <shichao.an@nyu.edu>
2014-05-21 23:12:49 -0400

    Added DFS solution

--- chapter9/question9.2.py
+++ chapter9/question9.2.py
@@ -1,5 +1,17 @@
 from __future__ import print_function
 
+"""
+Imagine a robot sitting on the upper left corner of an X by Y grid. The robot
+can only move in two directions: right and down. How many possible paths are
+there for the robot to go from (0,0) to (X, Y)?
+
+FOLLOW UP
+
+Imagine certain spots are "off limits", such that the robot cannot step on
+them. Design an algorithm to find a path for the robot from the top left to
+the bottom right.
+"""
+
 
 def factorial(n):
     if n == 0:
@@ -19,21 +31,32 @@ def get_num_paths(grid):
 
 
 def get_path(x, y, path, t, grid):
-    """Top-Down DP"""
+    """Top-Down DP, O(mn)"""
     if (x, y) in t:
         return t[(x, y)]
     path.append((x, y))
     if x == 0 and y == 0:
         return True
     success = False
-    if x >= 1 and grid[x - 1][y] != 1:
-        success = get_path(x - 1, y, path, t, grid)
-    if not success and y >= 1 and grid[x][y - 1] != 1:
+    if y >= 1 and grid[y - 1][x] != 1:
         success = get_path(x, y - 1, path, t, grid)
+    if not success and x >= 1 and grid[y][x - 1] != 1:
+        success = get_path(x - 1, y, path, t, grid)
     if not success:
         path.pop()
     t[(x, y)] = success
     return success
+
+
+def get_path2(x, y, path, grid, m, n):
+    """DFS, O(mn)"""
+    if x > m - 1 or y > n - 1 or len(path) == m + n - 1:
+        return
+    else:
+        if grid[y][x] != 1:
+            path.append((x, y))
+            get_path2(x + 1, y, path, grid, m, n)
+            get_path2(x, y + 1, path, grid, m, n)
 
 
 def _test():
@@ -46,8 +69,9 @@ def _print():
     grid = [
         [0, 0, 1, 0],
         [1, 0, 1, 0],
-        [1, 0, 0, 0],
+        [1, 0, 1, 0],
         [0, 0, 0, 0],
+        [1, 1, 1, 0],
     ]
     path1 = []
     t1 = {}
@@ -55,6 +79,9 @@ def _print():
     x = len(grid[0]) - 1
     get_path(x, y, path1, t1, grid)
     print(path1)
+    path2 = []
+    get_path2(0, 0, path2, grid, len(grid[0]), len(grid))
+    print(path2)
 
 
 if __name__ == '__main__':
