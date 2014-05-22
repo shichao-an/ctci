255aca0f819bfd6801ad7d18ac59fd3d2fc74958
Shichao An <shichao.an@nyu.edu>
2014-05-21 20:42:53 -0400

    Added question title

--- chapter9/question9.1.py
+++ chapter9/question9.1.py
@@ -1,5 +1,11 @@
 from __future__ import print_function
 
+"""
+A child is running up a staircase with n steps, and can hop either 1 step, 2
+steps, or 3 steps at a time. Implement a method to count how many possible
+ways the child can run up the stairs.
+"""
+
 
 def count_ways(n):
     """DP, Iterative"""
